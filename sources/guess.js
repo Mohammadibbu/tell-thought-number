6d451a900989fc1a4132a6ae95ef95cb56bf7b57
Muhamed ibrahim <92036859+Mohammadibbu@users.noreply.github.com>
2024-12-03 21:50:17 +0530

    Update guess.js

--- sources/guess.js
+++ sources/guess.js
@@ -1,41 +1,28 @@
-// Helper functions for vibration and audio
-function playVibration(pattern) {
-  navigator.vibrate(pattern);
+// check online offline using online offline attribute
+function on() {
+  document.getElementById("center").style.display = "block";
+  document.getElementById("online").style.display = "block";
+  document.getElementById("offline").style.display = "none";
+  navigator.vibrate([50, 100, 200]);
+  setTimeout(() => {
+    document.getElementById("online").style.display = "none";
+  }, 3000);
 }
-function playAudio(src) {
-  const audio = new Audio(src);
-  audio.play();
-}
-
-// Online/Offline handlers
-function handleOnline() {
-  toggleVisibility("center", true);
-  toggleVisibility("online", true);
-  toggleVisibility("offline", false);
-  playVibration([50, 100, 200]);
-  setTimeout(() => toggleVisibility("online", false), 3000);
-}
-function handleOffline() {
-  playVibration([50, 200, 200, 200]);
-  toggleVisibility("center", false);
-  toggleVisibility("offline", true);
+function of() {
+  navigator.vibrate([50, 200, 200, 200]);
   Audiohelp.pause();
+  document.getElementById("center").style.display = "none";
+  document.getElementById("offline").style.display = "block";
 }
+//assign variables
+let loader = document.getElementById("loader");
+let restart = document.getElementById("restart");
 
-// Utility function to toggle element visibility
-function toggleVisibility(id, show) {
-  document.getElementById(id).style.display = show ? "block" : "none";
-}
-
-// Initialize variables
-const loader = document.getElementById("loader");
-const restart = document.getElementById("restart");
-
-const stepone = document.getElementById("step1");
-const steptwo = document.getElementById("step2");
-const stepthree = document.getElementById("step3");
-const stepfour = document.getElementById("step4");
-
+let stepone = document.getElementById("step1");
+var steptwo = document.getElementById("step2");
+let stepthree = document.getElementById("step3");
+let stepfour = document.getElementById("step4");
+// var resultbtn=document.getElementById("resultbtn");
 let stp1y = Number(document.getElementById("step1y").value);
 let stp1n = Number(document.getElementById("step1n").value);
 let stp2y = Number(document.getElementById("step2y").value);
@@ -44,110 +31,173 @@ let stp3y = Number(document.getElementById("step3y").value);
 let stp3n = Number(document.getElementById("step3n").value);
 let stp4y = Number(document.getElementById("step4y").value);
 let stp4n = Number(document.getElementById("step4n").value);
+// click sound
+const click = new Audio("sounds/click.mp3");
 
-const clickSound = "sounds/click.mp3";
-const helpAudio = new Audio("sounds/help.mp3");
-
-// Step navigation functions
-function start() {
-  playVibration([50]);
-  playAudio(clickSound);
-  toggleVisibility("step1", true);
-  toggleVisibility("mainpage", false);
-  helpAudio.play();
+// Audiohelp
+const Audiohelp = new Audio("sounds/help.mp3");
+function st() {
+  navigator.vibrate([50]);
+  click.play();
+  document.getElementById("step1").style.display = "block";
+  document.getElementById("mainpage").style.display = "none";
+  Audiohelp.play();
 }
+//restart
 function reset() {
-  playVibration([50]);
-  playAudio(clickSound);
-  toggleVisibility("loader", true);
-  toggleVisibility("ans", false);
-  toggleVisibility("restart", false);
+  navigator.vibrate([50]);
+  click.play();
+  loader.style.display = "block";
+  document.getElementById("ans").style.display = "none";
+  restart.style.display = "none";
   setTimeout(() => {
-    toggleVisibility("loader", false);
+    loader.style.display = "none";
     location.reload();
   }, 1000);
 }
 
-// Step handlers
-function handleStep(stepNum, value) {
-  playVibration([50]);
-  playAudio(clickSound);
-  helpAudio.pause();
+// step1
+function stpy1() {
+  navigator.vibrate([50]);
+  Audiohelp.pause();
+  click.play();
+  yc1 = stp1y;
+  steptwo.style.display = "block";
+  stepone.style.display = "none";
+}
+function stpn1() {
+  Audiohelp.pause();
+  navigator.vibrate([50]);
+  click.play();
+  yc1 = stp1n;
+  steptwo.style.display = "block";
+  stepone.style.display = "none";
+}
+// step2
+function stpy2() {
+  navigator.vibrate([50]);
+  yc2 = stp2y;
+  click.play();
+  stepthree.style.display = "block";
+  steptwo.style.display = "none";
+}
+function stpn2() {
+  navigator.vibrate([50]);
+  click.play();
+  yc2 = stp1n;
+  stepthree.style.display = "block";
+  steptwo.style.display = "none";
+}
+//step3
+function stpy3() {
+  navigator.vibrate([50]);
+  click.play();
+  yc3 = stp3y;
+  stepfour.style.display = "block";
+  stepthree.style.display = "none";
+}
+function stpn3() {
+  navigator.vibrate([50]);
+  click.play();
+  stepfour.style.display = "block";
+  stepthree.style.display = "none";
+  yc3 = stp3n;
+}
+//step4
+function stpy4() {
+  navigator.vibrate([50]);
+  yc4 = stp4y;
+  click.play();
+  // resultbtn.style.display = "block";
+  stepfour.style.display = "none";
+  loader.style.display = "block";
+  setTimeout(answer, 1500);
+}
+function stpn4() {
+  navigator.vibrate([50]);
+  click.play();
+  loader.style.display = "block";
 
-  switch (stepNum) {
-    case 1:
-      yc1 = value;
-      toggleVisibility("step2", true);
-      toggleVisibility("step1", false);
+  // resultbtn.style.display = "block";
+  stepfour.style.display = "none";
+  yc4 = stp4n;
+
+  setTimeout(answer, 1500);
+}
+
+//answer
+function answer() {
+  // resultbtn.style.display = "none";
+  loader.style.display = "none";
+  restart.style.display = "block";
+
+  var result = yc1 + yc2 + yc3 + yc4;
+  console.log(result);
+  // function sound(){
+  // var snd = new Audio('onru.mp3')//wav is also supported
+  //  snd.play()}
+  //using switch case
+  switch (result) {
+    case 0:
+      answer = "'FIVE'";
+      new Audio("sounds/five.mp3").play();
       break;
-    case 2:
-      yc2 = value;
-      toggleVisibility("step3", true);
-      toggleVisibility("step2", false);
+    case 10:
+      answer = "'FOUR'";
+      new Audio("sounds/four.mp3").play();
       break;
-    case 3:
-      yc3 = value;
-      toggleVisibility("step4", true);
-      toggleVisibility("step3", false);
+    case 20:
+      answer = "'SIX'";
+      new Audio("sounds/six.mp3").play();
       break;
-    case 4:
-      yc4 = value;
-      toggleVisibility("step4", false);
-      toggleVisibility("loader", true);
-      setTimeout(evaluateAnswer, 1500);
+    case 30:
+      answer = "'ZERO'";
+      new Audio("sounds/zero.mp3").play();
       break;
+    case 40:
+      answer = "'ONE'";
+      new Audio("sounds/one.mp3").play();
+      break;
+    case 50:
+      answer = "'TWO'";
+      new Audio("sounds/two.mp3").play();
+      break;
+    case 60:
+      answer = "'TEN'";
+      new Audio("sounds/ten.mp3").play();
+      break;
+    case 70:
+      answer = "'THREE'";
+      new Audio("sounds/three.mp3").play();
+      break;
+    case 80:
+      answer = "'NINE'";
+      new Audio("sounds/nine.mp3").play();
+      break;
+    case 90:
+      answer = "'SEVEN'";
+      new Audio("sounds/seven.mp3").play();
+      break;
+    case 100:
+      answer = "'EIGHT'";
+      new Audio("sounds/eight.mp3").play();
   }
+
+  document.getElementById(
+    "ans"
+  ).innerHTML = `<h4 class="lineLeft">The Number You Thought of Was<br><br><i class="glow">${answer}</i></h4>`;
 }
 
-// Answer evaluation
-function evaluateAnswer() {
-  toggleVisibility("loader", false);
-  toggleVisibility("restart", true);
-
-  const result = yc1 + yc2 + yc3 + yc4;
-  const answers = {
-    0: "FIVE",
-    10: "FOUR",
-    20: "SIX",
-    30: "ZERO",
-    40: "ONE",
-    50: "TWO",
-    60: "TEN",
-    70: "THREE",
-    80: "NINE",
-    90: "SEVEN",
-    100: "EIGHT",
-  };
-
-  const soundMap = {
-    FIVE: "sounds/five.mp3",
-    FOUR: "sounds/four.mp3",
-    SIX: "sounds/six.mp3",
-    ZERO: "sounds/zero.mp3",
-    ONE: "sounds/one.mp3",
-    TWO: "sounds/two.mp3",
-    TEN: "sounds/ten.mp3",
-    THREE: "sounds/three.mp3",
-    NINE: "sounds/nine.mp3",
-    SEVEN: "sounds/seven.mp3",
-    EIGHT: "sounds/eight.mp3",
-  };
-
-  const answer = answers[result] || "UNKNOWN";
-  playAudio(soundMap[answer] || "");
-  document.getElementById("ans").innerHTML = `
-    <h4 class="lineLeft">The Number You Thought of Was<br><br><i class="glow">${answer}</i></h4>
-  `;
-}
-
-// Feedback confirmation
+// feedback confirmation
 function feedback() {
-  if (confirm("Opening Gmail or Email")) {
+  if (confirm("opening Gmail or Email")) {
     document.getElementById("fb").href = "mailto:mohammmadibbudummyacc235@gmail.com";
-    playVibration([50, 100, 50]);
-    setTimeout(() => (document.getElementById("fb").href = "#"), 2000);
+    navigator.vibrate([50, 100, 50]);
+    setTimeout(() => {
+      document.getElementById("fb").href = "#";
+    }, 2000);
   } else {
-    playVibration([100]);
     document.getElementById("fb").href = "#";
+    navigator.vibrate([100]);
   }
 }
